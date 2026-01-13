afed75f6299c39c7cd240276e767778a533e669f
“Rahmouni <rahmouniyasser77@gmail.com>
2026-01-13 10:39:27 +0100

    get backend ready for deployement

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(PORT, '0.0.0.0', () =>
+    app.listen(PORT, () =>
       console.log(`Server running on port ${PORT}`)
     );
   })
